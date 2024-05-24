142801fe01595f8a636b58fe14f71206a5f646d5
Leon Liu <thisisleonliu@gmail.com>
2024-05-25 03:33:16 +0800

    fix paddle sandbox

--- app/hooks/usePaddle.tsx
+++ app/hooks/usePaddle.tsx
@@ -5,9 +5,10 @@ import { useEffect, useState } from "react";
 
 export default function usePaddle() {
   const [paddle, setPaddle] = useState<Paddle>();
+  console.log(process.env.NEXT_PUBLIC_PADDLE_SANDBOX === 'production' ? "production" : "sandbox")
   useEffect(() => {
     initializePaddle({
-      environment: process.env.NEXT_PUBLIC_PADDLE_SANDBOX! ? "production" : "sandbox",
+      environment: process.env.NEXT_PUBLIC_PADDLE_SANDBOX === 'production' ? "production" : "sandbox",
       token: process.env.NEXT_PUBLIC_PADDLE_CLIENT_TOKEN!,
       // seller: Number(process.env.NEXT_PUBLIC_PADDLE_SELLER_ID),
     } as unknown as InitializePaddleOptions).then((paddleInstance: Paddle | undefined) => {
